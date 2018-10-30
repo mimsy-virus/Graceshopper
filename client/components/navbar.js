dfa733f6a3d696dc90f097c10bb9ebb9881d20df
ryhoneydew <lucia.yang88@gmail.com>
2018-10-30 13:02:51 -0400

    NavBar finished

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
     <h1>Grace Shopper</h1>
     <nav>
@@ -12,15 +12,22 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          
+          <Link to="products">Products</Link>
+          <Link to="account">Account</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          <Link to="cart">Cart</Link>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/home">Home</Link>
+          <Link to="products">Products</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="cart">Cart</Link>
         </div>
       )}
     </nav>
