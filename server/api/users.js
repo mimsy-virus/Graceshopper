7449b07938564aa94ee23f6638dbac573ecf603d
ryhoneydew <lucia.yang88@gmail.com>
2018-11-01 12:35:22 -0400

    Added api security middleware

--- server/api/users.js
+++ server/api/users.js
@@ -2,40 +2,47 @@ const router = require('express').Router()
 const { User } = require('../db/models')
 module.exports = router
 
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const users = await User.findAll({
-//       // explicitly select only the id and email fields - even though
-//       // users' passwords are encrypted, it won't help if we just
-//       // send everything to anyone who asks!
-//       attributes: [
-//         'id',
-//         'firstName',
-//         'lastName',
-//         'email',
-//         'phone',
-//         'userAddress',
-//         'userCity',
-//         'userState',
-//         'userZipCode',
-//         'isAdmin'
-//       ]
-//     })
-//     res.json(users)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id)
-    if (!user) return res.status(404).send('Not found')
-    res.json(user)
-  } catch (err) {
-    next(err)
+router.get(
+  '/',
+  function(req, res, next) {
+    console.log(req.user.isAdmin)
+    if (req.user.isAdmin) {
+      next()
+    } else {
+      res.status(401).send('Access Denied!')
+    }
+  },
+  async (req, res, next) => {
+    try {
+      const users = await User.findAll()
+      res.json(users)
+    } catch (err) {
+      next(err)
+    }
   }
-})
+)
+
+router.get(
+  '/:id',
+  function(req, res, next) {
+    console.log(req.user.isAdmin)
+    if (req.user.isAdmin) {
+      next()
+    } else {
+      res.status(401).send('Access Denied!')
+    }
+  },
+  async (req, res, next) => {
+    try {
+      console.log(req.user)
+      const user = await User.findById(req.params.id)
+      if (!user) return res.status(404).send('Not found')
+      res.json(user)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
 
 router.post('/', async (req, res, next) => {
   try {
@@ -56,12 +63,23 @@ router.put('/:id', async (req, res, next) => {
   }
 })
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id)
-    await user.destroy()
-    res.json(user)
-  } catch (err) {
-    next(err)
+router.delete(
+  '/:id',
+  function(req, res, next) {
+    console.log(req.user.isAdmin)
+    if (req.user.isAdmin) {
+      next()
+    } else {
+      res.status(401).send('Access Denied!')
+    }
+  },
+  async (req, res, next) => {
+    try {
+      const user = await User.findById(req.params.id)
+      await user.destroy()
+      res.json(user)
+    } catch (err) {
+      next(err)
+    }
   }
-})
+)
