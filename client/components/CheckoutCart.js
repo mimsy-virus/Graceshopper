0ce8c09986bf0152c966092f87d1e23364371e4b
Chris Li <chrisli2017@gmail.com>
2018-11-01 12:39:54 -0400

    created cart component

--- client/components/CheckoutCart.js
+++ client/components/CheckoutCart.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import getCartFromServer from '../store/cart'
+import addItemToServer from '../store/cart'
+import updateItemToServer from '../store/cart'
+import removeItemFromServer from '../store/cart'
+import clearCartFromServer from '../store/cart'
 
 class CheckoutCart extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      cart: []
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     cart: {}
+  //   }
+  // }
 }
+
+const mapStateToProps = state => ({
+  userCart: this.state.cart.userCart
+})
+
+const mapDispatchToProps = dispatch => ({
+  getCartFromServer: userId => dispatch(getCartFromServer(userId)),
+  addItemToServer: (userId, item) => dispatch(addItemToServer(userId, item)),
+  updateItemToServer: () => dispatch(),
+  removeItemFromServer: () => dispatch(),
+  clearCartFromServer: () => dispatch()
+})
