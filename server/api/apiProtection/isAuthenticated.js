74e9e3ed0304104f2e83a6ca6dd61897ccd3d518
ryhoneydew <lucia.yang88@gmail.com>
2018-11-05 15:05:23 -0500

    Update-stripeKey

--- server/api/apiProtection/isAuthenticated.js
+++ server/api/apiProtection/isAuthenticated.js
@@ -1,3 +1,3 @@
 const isAutheticated = (req, userId) =>
   Number(req.user.dataValues.id) === Number(userId) || req.user.isAdmin
-module.exports = { isAutheticated }
+module.exports = isAutheticated
