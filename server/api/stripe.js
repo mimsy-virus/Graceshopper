0b7b08e736fc5b4e8992569d6e436ba1e8c9f2ff
Chris Li <chrisli2017@gmail.com>
2018-11-05 15:20:16 -0500

    temp

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const stripe = require('stripe')('sk_test_ZGEpF0zpWch6FYLWY3xYMNGA')
+const stripe = require('stripe')
+// ('sk_test_ZGEpF0zpWch6FYLWY3xYMNGA')
 const { Order } = require('../db/models')
 module.exports = router
 
