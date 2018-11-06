6d47fc322bd98d82abb4913800f8e6bf110198c1
ryhoneydew <lucia.yang88@gmail.com>
2018-11-05 19:51:13 -0500

    Update- improve the protections on all routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const { User } = require('../db/models')
-
+const { isAuthenticated } = require('./apiProtection/isAuthenticated')
+const { ifIsAdmin } = require('./apiProtection/isAdmin')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', ifIsAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
@@ -23,6 +24,16 @@ router.get('/', async (req, res, next) => {
       ]
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', isAuthenticated, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) return res.status(404).send('Not found')
+    res.json(user)
   } catch (err) {
     next(err)
   }
