8059d67ce2c03560afd7bfdbe5f5a9461c9888cf
ryhoneydew <lucia.yang88@gmail.com>
2018-11-06 12:45:04 -0500

    Removed- user routes spec

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,70 +1,71 @@
-/* global describe beforeEach it */
+// /* global describe beforeEach it */
 
-const { expect } = require('chai')
-const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
-const User = db.model('user')
+// const { expect } = require('chai')
+// const request = require('supertest')
+// const db = require('../db')
+// const app = require('../index')
+// const User = require('../db/models/user')
 
 // describe('User routes', () => {
 //   beforeEach(() => {
 //     return db.sync({ force: true })
 //   })
-//   const adminCredentials = User.create({
+//   const adminCredential = {
 //     email: 'sponge@bob.com',
 //     password: 'thePassword1',
 //     isAdmin: true
-//   })
-//   const regularUserCredentials = {
+//   }
+
+//   const regularCredential = {
 //     email: 'cody@puppybook.com',
 //     password: 'thePassword2'
 //   }
 
-//   // beforeEach(async () => {
-//   //   const adminResponseTest = await User.create(adminCredentials)
-//   //   const regularResponseTest = await User.create(regularUserCredentials)
-//   //   expect(adminResponseTest.statusCode).to.equal(200)
-//   //   expect(regularResponseTest.statusCode).to.equal(200)
-//   // })
+//   beforeEach(async () => {
+//     try {
+//       const adminUser = await User.create(adminCredential)
+//       const regularUser = await User.create(regularCredential)
+//     } catch (err) {
+//       throw err
+//     }
+//   })
 
+//   const loggedInRegularUser = request.agent(app)
+//   const loggedInAdminUser = request.agent(app)
 //   describe('/api/users/', () => {
-//     const regularUser = request.agent(app)
-//     const adminUser = request.agent(app)
-
-//     before(done => {
-//       adminUser
-//         .post('/auth/login')
-//         .send({
-//           email: 'brrp@email.com',
-//           password: '123'
+//     before(async () => {
+//       try {
+//         const response = await loggedInAdminUser.post('/auth/login').send({
+//           email: 'sponge@bob.com',
+//           password: 'thePassword1'
 //         })
-//         .end(function(err, response) {
-//           expect(response.statusCode).to.equal(200)
-//           if (err) throw err
-//           done()
+//         console.log('!!!!!!', response.body)
+//         expect(response.statusCode).to.equal(200)
+//       } catch (err) {
+//         throw err
+//       }
+//     })
+//     describe('GET /api/users', async () => {
+//       const response = await loggedInAdminUser.get('/api/users').expect(200)
+//       expect(response.body.length).to.equal(8)
+//     })
+//   })
+
+//   describe('Regular user GET /api/users', () => {
+//     before(async () => {
+//       try {
+//         const response = await loggedInRegularUser.post('/auth/login').send({
+//           email: 'cody@puppybook.com',
+//           password: 'thePassword2'
 //         })
+//         //console.log('!!!!!!', response.body)
+//         expect(response.statusCode).to.equal(200)
+//       } catch (err) {
+//         throw err
+//       }
 //     })
-
-//     describe('GET /api/users', () => {
-//       it('gives Admin users access', async () => {
-//         console.log('!!!!', adminUser)
-//         const res = await adminUser
-//           .get('/api/users')
-//           .expect('Content-Type', /json/)
-//           .expect(200)
-//         console.log('!!!!', res)
-//         expect(res.body).to.be.an.instanceOf(Array)
-//         expect(res.body).to.have.length(2)
-//       })
+//     it('Not allow regular user to get users info', async () => {
+//       const response = await loggedInRegularUser.get('/api/users').expect(401)
 //     })
-
-//     // it('GET /api/users', async () => {
-//     //   const res = await request(app)
-//     //     .get('/api/users')
-//     //     .expect(200)
-
-//     //   expect(res.body).to.be.an('array')
-//     //   expect(res.body[0].email).to.be.equal(codysEmail)
-//     // })
-//   }) // end describe('/api/users')
-// }) // end describe('User routes')
+//   })
+// })
